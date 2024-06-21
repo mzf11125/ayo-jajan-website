89619794098bd97063ac06ced297518981944833
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2024-06-22 02:25:24 +0700

    edit style

--- ayo-jajan/src/app/page.tsx
+++ ayo-jajan/src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main
       className="flex min-h-screen flex-col items-center justify-center p-24"
-      style={{ background: "rgba(255, 255, 0, 0.7)" }}
+      style={{ background: "rgba(255, 255, 0, 0.78)" }}
     >
       <section>
-        <h1 className="text-center font-bold text-4xl">
+        <h1 className="text-center font-bold text-5xl">
           This is Ayo Jajan, an app to track local street vendors
         </h1>
       </section>
