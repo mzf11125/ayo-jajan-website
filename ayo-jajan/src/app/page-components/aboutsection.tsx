9c88840c4086157cdbbbf715d0e00f5b4839b75a
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2024-09-13 22:15:28 +0700

    testing

--- ayo-jajan/src/app/page-components/aboutsection.tsx
+++ ayo-jajan/src/app/page-components/aboutsection.tsx
@@ -8,7 +8,7 @@ export default function AboutSection() {
       <section id="about" className="mt-20">
         <h2 className="text-2xl font-bold mb-4">About Ayo Jajan</h2>
         <p>
-          Ayo Jajan is on a mission to promote local street food, including food
+          Ayo Jaja is on a mission to promote local street food, including food
           carts and food trucks. We believe in supporting small businesses and
           showcasing the diverse culinary experiences that can be found on the
           streets. Through our platform, you can track local street vendors in
